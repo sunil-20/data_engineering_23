c023fe1681d549e7d852a90aff74286ba0f02bfe
SK <sunil.kafle@hotmail.com>
2023-11-17 14:59:11 -0500

    caregiver final

--- GenSynthetic/daycare_tables/caregiver.py
+++ GenSynthetic/daycare_tables/caregiver.py
@@ -3,7 +3,7 @@ from random import seed
 import pandas as pd
 from faker import Faker
 from .uid_generator import UIDGenerator
-
+from datetime import datetime, timedelta
 
 class Caregiver:
     def __init__(self, x):
@@ -24,10 +24,11 @@ class Caregiver:
             data.loc[i, "caregiver_id"] = self.uid_gen.generate_uid(digits_id)
             data.loc[i, "caregiver_name"] = self.f.name()
             data.loc[i, "certificate_type"] = self.f.random_element(elements=self.certificte_type)
-            data.loc[i, "certificate_expiration_date"]= self.f.date_future(end_date='+10y', tzinfo=None)
-            
+            today = datetime.today()
+            future_date = today.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=self.f.random_int(min=1, max=3650)) 
+            data.loc[i, "certificate_expiration_date"]= future_date
         # convert to datetime
-        data["certification_expiration_date"] = pd.to_datetime(data["certification_expiration_date"])
+        data["certificate_expiration_date"] = pd.to_datetime(data["certificate_expiration_date"])
 
         return data
     
--- GenSynthetic/main.py
+++ GenSynthetic/main.py
@@ -3,15 +3,15 @@ import pandas as pd
 from daycare_tables.kid import Kid
 from daycare_tables.caregiver import Caregiver
 
-# generate kid table
+#generate kid table
 kid_instance = Kid(x=100)
 kid_data = kid_instance.generate_data()
 #save_to_CSV
-kid_data.to_csv("kid.csv", index = False)
+kid_data.to_csv("csv_tables/kid.csv", index = False)
 
 
 #generate caregiver table
 caregiver_instance = Caregiver(x=20)
 caregiver_data = caregiver_instance.generate_data()
 #save to CSV
-caregiver_data.to_csv("caregiver.csv", index = False)
+caregiver_data.to_csv("csv_tables/caregiver.csv", index = False)
