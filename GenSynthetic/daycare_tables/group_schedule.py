4986625c52c08484d36d1071a7b9979baac8e323
SK <sunil.kafle@hotmail.com>
2023-11-17 17:22:46 -0500

    group_schedule added

--- GenSynthetic/daycare_tables/group_schedule.py
+++ GenSynthetic/daycare_tables/group_schedule.py
@@ -1,1 +1,24 @@
 # Group_schedule table.
+from random import seed
+import pandas as pd
+from faker import Faker
+from .uid_generator import UIDGenerator
+from datetime import datetime, timedelta
+
+class Group_schedule:
+    def __init__(self, x):
+        self.x = x
+        self.f = Faker()
+        self.uid_gen = UIDGenerator(self.f)
+    
+
+
+        #seed
+        seed(2)
+
+    # erdlab import
+#     Group_schedule {
+# 	group_schedule_id integer pk null increments
+# 	number integer
+# 	course_id integer > Course.course_id
+# }
